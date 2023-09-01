53eaf2c12632d4803d840dc77abae71c9d9d8d3b
Gamer2020 <lostatlife2020@hotmail.com>
2023-09-01 00:00:20 -0400

    Download Pokemon Images.

--- data_generators/download_pokemon_images.py
+++ data_generators/download_pokemon_images.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 POKEMON_BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
-FORM_BASE_URL = "https://pokeapi.co/api/v2/pokemon-form/"
+POKEMON_SPECIES_URL = "https://pokeapi.co/api/v2/pokemon-species/"
 IMAGE_SAVE_PATH = "./src/img/pokemon/"
 
 def download_image(image_url, index):
@@ -12,28 +12,30 @@ def download_image(image_url, index):
         with open(image_path, 'wb') as file:
             file.write(image_response.content)
 
-def download_form_images(pokemon_id):
-    response = requests.get(FORM_BASE_URL + str(pokemon_id))
-    if response.status_code == 200:
-        form_data = response.json()
-        form_id = form_data['id']
-        image_url = form_data['sprites']['front_default']
-        download_image(image_url, form_id)
-
 def main():
     # Ensure the directory exists
     if not os.path.exists(IMAGE_SAVE_PATH):
         os.makedirs(IMAGE_SAVE_PATH)
 
-    for i in range(1, 650):  # Assuming 649 Pokémon for gens 1-5
-        response = requests.get(POKEMON_BASE_URL + str(i))
-        if response.status_code == 200:
-            data = response.json()
-            image_url = data['sprites']['front_default']
-            download_image(image_url, i)
+    # Get the total count of Pokémon
+    response = requests.get(POKEMON_BASE_URL)
+    total_count = response.json()['count']
 
-        # Download form images
-        download_form_images(i)
+    # Loop through all Pokémon species
+    for i in range(1, total_count + 1):
+        species_response = requests.get(POKEMON_SPECIES_URL + str(i))
+        if species_response.status_code == 200:
+            species_data = species_response.json()
+
+            # Loop through all varieties of the Pokémon species
+            for variety in species_data['varieties']:
+                pokemon_id = variety['pokemon']['url'].split('/')[-2]  # Extract ID from URL
+                pokemon_response = requests.get(POKEMON_BASE_URL + pokemon_id)
+                if pokemon_response.status_code == 200:
+                    pokemon_data = pokemon_response.json()
+                    image_url = pokemon_data['sprites']['front_default']
+                    if image_url:
+                        download_image(image_url, pokemon_id)
 
 if __name__ == "__main__":
     main()
